9a611b30d3dfd216bd645371dec5c0643c582260
Hamroliyev <90793925+Hamroliyev@users.noreply.github.com>
2022-02-24 15:43:50 +0300

    Add project files.

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static int binarySearch(int[] arr, int l, int r, int x)
+        {
+            if (r >= l)
+            {
+                int mid = l + (r - l) / 2;
+
+                if (arr[mid] == x)
+                    return mid;
+
+                if (arr[mid] > x)
+                    return binarySearch(arr, l, mid - 1, x);
+
+                return binarySearch(arr, mid + 1, r, x);
+            }
+
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 3, 4, 10, 40 };
+            int n = arr.Length;
+            int x = 10;
+
+            int result = binarySearch(arr, 0, n - 1, x);
+
+            if (result == -1)
+                Console.WriteLine("Element mavjud emas");
+            else
+                Console.WriteLine("Topilgan element indeksi "
+                                  + result);
+            Console.ReadKey();
+        }
+    }
+}
